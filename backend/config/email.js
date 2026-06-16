acb5b8492f8e952d56aed9856c50a2e9875f8c4c
PREM-xd <premfc9@gmail.com>
2026-06-16 11:23:43 +0530

    update env

--- backend/config/email.js
+++ backend/config/email.js
@@ -1,18 +1,14 @@
-const nodemailer =
-  require("nodemailer");
+const nodemailer = require("nodemailer");
 
-const transporter =
-  nodemailer.createTransport({
-    service: "gmail",
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
 
-    auth: {
-      user:
-        process.env.EMAIL_USER,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-      pass:
-        process.env.EMAIL_PASS,
-    },
-  });
-
-module.exports =
-  transporter;
+module.exports = transporter;
