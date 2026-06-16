55b8d94f9803f4160a0c6d874b089985f4b055a5
PREM-xd <premfc9@gmail.com>
2026-06-16 11:27:39 +0530

    update env

--- backend/config/email.js
+++ backend/config/email.js
@@ -4,11 +4,18 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+});
+
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log("EMAIL VERIFY ERROR:", error);
+  } else {
+    console.log("EMAIL SERVER READY");
+  }
 });
 
 module.exports = transporter;
