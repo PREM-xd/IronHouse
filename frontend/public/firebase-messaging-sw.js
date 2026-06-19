dd39e477e7da086d1804403e7a8e50ac33a03d19
PREM-xd <premfc9@gmail.com>
2026-06-19 22:25:04 +0530

    add background notifications

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -16,3 +16,23 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(
+  (payload) => {
+
+    console.log(
+      "Background Message:",
+      payload
+    );
+
+    self.registration.showNotification(
+      payload.notification.title,
+      {
+        body:
+          payload.notification.body,
+        icon: "/favicon.ico",
+      }
+    );
+
+  }
+);
