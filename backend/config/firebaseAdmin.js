b86d09f68341a12cc1d164edf1f2a64410cbfe7c
PREM-xd <premfc9@gmail.com>
2026-06-19 22:13:30 +0530

    updated new

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,14 +1,23 @@
 const admin = require("firebase-admin");
 
+console.log(
+  "FIREBASE ENV EXISTS:",
+  !!process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+console.log(
+  "ADMIN:",
+  admin
+);
+
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 admin.initializeApp({
-  credential:
-    admin.credential.cert(
-      serviceAccount
-    ),
+  credential: admin.credential.cert(
+    serviceAccount
+  ),
 });
 
 module.exports = admin;
