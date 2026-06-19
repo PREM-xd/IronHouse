6cb199aacb54aaa3e36acf066424059ed81d31cf
PREM-xd <premfc9@gmail.com>
2026-06-19 21:54:32 +0530

    updated new

--- backend/controllers/fcmController.js
+++ backend/controllers/fcmController.js
@@ -5,17 +5,40 @@ const saveFcmToken = async (
   res
 ) => {
   try {
-    await User.findByIdAndUpdate(
-      req.user.id,
-      {
-        fcmToken: req.body.token,
-      }
+
+    console.log(
+      "USER:",
+      req.user.id
+    );
+
+    console.log(
+      "FCM TOKEN:",
+      req.body.token
+    );
+
+    const user =
+      await User.findByIdAndUpdate(
+        req.user.id,
+        {
+          fcmToken: req.body.token,
+        },
+        {
+          new: true,
+        }
+      );
+
+    console.log(
+      "UPDATED USER:",
+      user
     );
 
     res.json({
       success: true,
     });
+
   } catch (error) {
+    console.log(error);
+
     res.status(500).json({
       message: error.message,
     });
